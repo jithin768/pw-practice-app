5b4f34a2928e167704c9bd9e737e50118f4fb188
Jithin Mathew <jithinmathew65@gmail.com>
2026-02-06 06:36:50 -0400

    Locator Syntax rules

--- tests/secondTest.spec.ts
+++ tests/secondTest.spec.ts
@@ -1,1 +1,33 @@
-console.log('This is the second test file')
+import {test} from '@playwright/test';
+
+
+test.beforeEach(async ({page}) => {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+
+   test('Locator syntax rules', async ({page}) => {
+
+    //By tag name
+    page.locator('input')  
+    //By id
+    page.locator('#inputEmail1')
+    //By class name
+    page.locator('.input-full-width')
+    //By attribute name
+    page.locator('[placeholder="Email"]')
+    //By attribute name and value
+    page.locator('[placeholder="Email"][type="email"]') 
+    //By class value
+    page.locator('[class="input-full-width size-medium shape-rectangle"]')
+    //By tag name and attribute with value
+    page.locator('input[placeholder="Email"]')
+    //By tag name, attribute with value and class value
+    page.locator('input[placeholder="Email"].input-full-width')
+
+   })
+
+ 
+
