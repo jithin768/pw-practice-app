1d3cb5c344e20b942b4c4f9ab84b4ea9043bb389
Jithin Mathew <jithinmathew65@gmail.com>
2026-02-07 15:52:45 -0400

    Parent Element Locators

--- tests/secondTest.spec.ts
+++ tests/secondTest.spec.ts
@@ -54,7 +54,7 @@ test.beforeEach(async ({page}) => {
 
    })
 
-   test('Child Element Locators', async ({page}) => {
+   test.skip('Child Element Locators', async ({page}) => {
 
     await page.locator('nb-card nb-radio :text-is("Option 1")').click()
     await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
@@ -62,6 +62,21 @@ test.beforeEach(async ({page}) => {
     await page.locator('nb-card').getByRole('button',{name:'SIGN IN '}).first().click()
 
    })
+
+   test('Locating Parent Elemnts',async({page})=>{
+
+    await page.locator('nb-card',{hasText:'Using the Grid'}).getByRole('textbox',{name:'Email'}).click()
+    await page.locator('nb-card',{has:page.locator('#inputEmail1')}).getByRole('textbox',{name:'Email'}).click()
+
+
+    await page.locator('nb-card').filter({hasText:'Basic form'}).getByRole('textbox',{name:'Email'}).click()
+    await page.locator('nb-card').filter({has:page.locator('.status-danger')}).getByRole('textbox',{name:'Password'}).click()
+
+    await page.locator('nb-card').filter({has:page.locator('nb-checkbox')}).filter({hasText:'Sign in'}).getByRole('textbox',{name:'Email'}).click()
+
+    await page.locator(':text-is("Using the Grid")').locator('..').getByRole('button',{name:'SIGN IN '}).click()
+   })
+
 
 
  
